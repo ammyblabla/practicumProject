1f585b40cb19e869f20c15376f1ac1642894d1d6
Chayanin Tongphasook <chayanin2007@gmail.com>
2016-10-30 14:23:30 +0700

    first commit

---
+++ core/src/com/mygdx/game/GameScreen.java
@@ -0,0 +1,12 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class GameScreen extends ScreenAdapter {
+
+	private ClearThisSky clearThisSky;
+	 
+    public GameScreen(ClearThisSky clearThisSky) {
+        this.clearThisSky = clearThisSky;
+    }
+}
